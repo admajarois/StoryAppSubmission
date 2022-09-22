726032f065aed1af938287181bf88b25c538b4b1
admajarois <rois.admj@gmail.com>
2022-09-22 16:47:09 +0700

    add some activity

---
+++ app/src/main/java/com/admaja/storyappsubmission/view/signup/SignUpActivity.kt
@@ -0,0 +1,12 @@
+package com.admaja.storyappsubmission.view.signup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.admaja.storyappsubmission.R
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
