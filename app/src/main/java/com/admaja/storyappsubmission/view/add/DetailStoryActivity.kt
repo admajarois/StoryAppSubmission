8ce968bb2d08e9714f8ba152e5267e282ac8bab5
admaja <rois.admj@gmail.com>
2022-09-24 18:40:06 +0700

    add some fixing in detail story

--- app/src/main/java/com/admaja/storyappsubmission/view/add/DetailStoryActivity.kt
+++ app/src/main/java/com/admaja/storyappsubmission/view/add/DetailStoryActivity.kt
@@ -2,6 +2,7 @@ package com.admaja.storyappsubmission.view.add
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.admaja.storyappsubmission.R
 
 class DetailStoryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
