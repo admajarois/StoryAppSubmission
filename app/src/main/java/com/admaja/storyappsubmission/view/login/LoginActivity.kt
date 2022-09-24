f1dea59b2fc061304c0ebaf5fa97bf59a5958b96
admaja <rois.admj@gmail.com>
2022-09-24 18:34:19 +0700

    add list item story

--- app/src/main/java/com/admaja/storyappsubmission/view/login/LoginActivity.kt
+++ app/src/main/java/com/admaja/storyappsubmission/view/login/LoginActivity.kt
@@ -3,10 +3,14 @@ package com.admaja.storyappsubmission.view.login
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.admaja.storyappsubmission.R
+import com.admaja.storyappsubmission.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(binding.root)
     }
 }
