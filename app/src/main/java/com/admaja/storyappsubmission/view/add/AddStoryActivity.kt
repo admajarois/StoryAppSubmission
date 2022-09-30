5514ace863688ae8f2839b7f6ba9e6c74c9ee01a
admajarois <rois.admj@gmail.com>
2022-09-30 21:57:30 +0700

    add some editing on add new story

--- app/src/main/java/com/admaja/storyappsubmission/view/add/AddStoryActivity.kt
+++ app/src/main/java/com/admaja/storyappsubmission/view/add/AddStoryActivity.kt
@@ -5,16 +5,92 @@ import android.os.Bundle
 import com.admaja.storyappsubmission.databinding.ActivityAddStoryBinding
 import java.io.File
 import android.Manifest
+import android.content.Intent
+import android.content.Intent.ACTION_GET_CONTENT
+import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
+import android.net.Uri
+import android.provider.MediaStore
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
+import com.admaja.storyappsubmission.R
+import com.admaja.storyappsubmission.utils.createCustomTempFile
+import com.admaja.storyappsubmission.utils.uriToFile
 
 class AddStoryActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityAddStoryBinding
     private var getFile: File? = null
+    private lateinit var currentPhotoPath: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAddStoryBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        if (!allPermissionGranted()) {
+            ActivityCompat.requestPermissions(
+                this,
+                REQUIRED_PERMISSIONS,
+                REQUEST_CODE_PERMISSIONS
+            )
+        }
+        binding.apply {
+            btOpenCamera.setOnClickListener {startCamera()}
+            btOpenGalerry.setOnClickListener { startGallery() }
+        }
+    }
+
+    private fun startGallery() {
+        val intent = Intent()
+        intent.action = ACTION_GET_CONTENT
+        intent.type = "image/*"
+        val chooser = Intent.createChooser(intent, R.string.choose_picture.toString())
+        launcherIntentGallery.launch(chooser)
+    }
+
+    private fun startCamera() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        intent.resolveActivity(packageManager)
+
+        createCustomTempFile(application).also {
+            val photoURI: Uri = FileProvider.getUriForFile(
+                this@AddStoryActivity,
+                "com.admaja.storyappsubmission",
+                it
+            )
+            currentPhotoPath = it.absolutePath
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+            launcherIntentCamera.launch(intent)
+
+        }
+
+    }
+
+    private val launcherIntentCamera = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {
+        if (it.resultCode == RESULT_OK) {
+            val myFile = File(currentPhotoPath)
+            getFile = myFile
+
+            val result = BitmapFactory.decodeFile(getFile?.path)
+            binding.ivAddStory.setImageBitmap(result)
+        }
+    }
+
+    private val launcherIntentGallery = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == RESULT_OK) {
+            val selectedImg: Uri = result.data?.data as Uri
+            val myFile = uriToFile(selectedImg, this@AddStoryActivity)
+            getFile = myFile
+            binding.ivAddStory.setImageURI(selectedImg)
+        }
     }
 
     override fun onRequestPermissionsResult(
@@ -23,11 +99,21 @@ class AddStoryActivity : AppCompatActivity() {
         grantResults: IntArray
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            Toast.makeText(
+                this,
+                "Not Getting Permission",
+                Toast.LENGTH_SHORT
+            ).show()
+            finish()
+        }
+    }
+
+    private fun allPermissionGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
     }
 
     companion object {
-        const val CAMERA_X_RESULT = 200
-
         private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
         private const val REQUEST_CODE_PERMISSIONS = 10
     }
