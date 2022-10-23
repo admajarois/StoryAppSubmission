adcddd2201a2ab8f5a07a205b71ce5917f7c9e65
admajarois <rois.admj@gmail.com>
2022-10-23 19:54:56 +0700

    menambahkan skenario

--- app/src/test/java/com/admaja/storyappsubmission/data/DataRepositoryTest.kt
+++ app/src/test/java/com/admaja/storyappsubmission/data/DataRepositoryTest.kt
@@ -26,7 +26,6 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mock
 import org.mockito.junit.MockitoJUnitRunner
-import kotlin.math.exp
 
 @ExperimentalCoroutinesApi
 @RunWith(MockitoJUnitRunner::class)
